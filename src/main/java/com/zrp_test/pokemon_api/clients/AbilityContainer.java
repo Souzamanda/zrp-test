de41b88c526ba0d7b246ffd4231e3ab7a64e88bb
Amanda Puttomatti <acsputtomatti@gmail.com>
2025-03-27 20:07:53 -0300

    chore: created Pokemon abilities entity and handler

---
+++ src/main/java/com/zrp_test/pokemon_api/clients/AbilityContainer.java
@@ -0,0 +1,13 @@
+package com.zrp_test.pokemon_api.clients;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.zrp_test.pokemon_api.entities.Ability;
+
+public class AbilityContainer {
+    @JsonProperty("ability")
+    private Ability ability;
+
+    public Ability getAbility() {
+        return ability;
+    }
+}
